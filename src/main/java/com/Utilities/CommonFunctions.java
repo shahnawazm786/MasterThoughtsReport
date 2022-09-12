ac51ed0ce37003339f6de560c19517b50a7563ea
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-09-12 21:35:38 +0530

    wait

--- src/main/java/com/Utilities/CommonFunctions.java
+++ src/main/java/com/Utilities/CommonFunctions.java
@@ -1,10 +1,15 @@
 package com.Utilities;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonFunctions {
+	static WebDriverWait wait;
 	public static boolean isElementDisplayed(WebElement element) {
 		if(element.isDisplayed())
 			return true;
@@ -19,5 +24,14 @@ public class CommonFunctions {
 			return false;
 	}
 	
+	public static void isElementClickable(WebDriver driver,WebElement element,long timeout)
+	{
+		wait=new WebDriverWait(driver,Duration.ofSeconds(timeout));
+		wait.pollingEvery(Duration.ofMillis(500)).until(ExpectedConditions.elementToBeClickable(element));
+	}
+	public static void isElementVisibile(WebDriver driver,WebElement element,long timeout) {
+		wait=new WebDriverWait(driver,Duration.ofSeconds(timeout));
+		wait.pollingEvery(Duration.ofMillis(500)).until(ExpectedConditions.invisibilityOf(element));
+	}
 
 }
